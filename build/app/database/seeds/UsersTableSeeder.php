63c7008c480751b516ba05f5de58cc09df687607
Yu YAGIHASHI <yagihashoooooo@gmail.com>
2018-09-27 04:04:43 +0900

    Fixed seeding config

--- build/app/database/seeds/UsersTableSeeder.php
+++ build/app/database/seeds/UsersTableSeeder.php
@@ -17,6 +17,8 @@ class UsersTableSeeder extends Seeder
             'name' => env('APP_ADMIN_USER', 'admin'),
             'password' => bcrypt(env('APP_ADMIN_PASSWORD', 'password')),
             'role_id' => 2,
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
         ]);
     }
 }
