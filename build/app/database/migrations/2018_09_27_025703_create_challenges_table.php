61bab92106c4e0a244efaa33a6941d34135f3d77
Yu YAGIHASHI <yagihashoooooo@gmail.com>
2018-09-27 03:22:05 +0900

    Added migrations

---
+++ build/app/database/migrations/2018_09_27_025703_create_challenges_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChallengesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('challenges', function (Blueprint $table) {
+            $table->uuid('id')->unique();
+            $table->string('title', 128)->unique();
+            $table->string('model_answer', 512);
+            $table->boolean('solved')->default(false);
+            $table->uuid('setter_id');
+            $table->uuid('file_id')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('challenges', function (Blueprint $table) {
+            //
+        });
+    }
+}
---
+++ build/app/database/migrations/2018_09_27_030354_create_submits_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubmitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('submits', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('payload', 512);
+            $table->uuid('user_id');
+            $table->uuid('challenge_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('submits');
+    }
+}
