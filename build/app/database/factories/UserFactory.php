083530d51ba73a661e8674e4af9a1d93503ff511
Yu YAGIHASHI <yagihashoooooo@gmail.com>
2018-09-27 19:28:00 +0900

    Fixed user factory

--- build/app/database/factories/UserFactory.php
+++ build/app/database/factories/UserFactory.php
@@ -5,6 +5,6 @@ use Faker\Generator as Faker;
 $factory->define(App\User::class, function (Faker $faker) {
     return [
         'name' => $faker->firstName(),
-        'password' => bcrypt($faker->sentence(rand(1,4))),
+        'password' => bcrypt($faker->password(8, 8)),
     ];
 });
