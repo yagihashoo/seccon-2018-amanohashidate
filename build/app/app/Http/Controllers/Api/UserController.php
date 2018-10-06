ac8c76fee463c5541c4b94e5771bcb5083b1c5c7
Yu YAGIHASHI <yagihashoooooo@gmail.com>
2018-10-06 17:04:02 +0900

    Started to implement APIs

---
+++ build/app/app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Input;
+use App\User;
+
+class UserController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function me()
+    {
+        return response(Auth::user())
+    }
+
+    public function index(Request $request)
+    {
+        $this->authorize('index', User::class);
+
+        $limit = (min(Input::get('limit'), 100) ?? 100) + 1;
+        $offset = Input::get('offset') ?? 0;
+
+        $users = User::limit($limit)->offset($offset)->orderBy('name')->get()->toArray();
+        $hasNext = (sizeof($users) === $limit && array_pop($users) !== null);
+
+        return response([
+            'users' => $users,
+            'hasNext' => $hasNext,
+        ]);
+    }
+
+    public function show(User $user)
+    {
+        return response($user);
+    }
+
+}
--- build/app/routes/api.php
+++ build/app/routes/api.php
@@ -13,6 +13,8 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+Route::resource('user', 'Api\UserController', ['only' => ['index', 'show']]);
+Route::get('me', 'Api\UserController@me');
