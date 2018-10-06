284336221b52c27a34f66a232eb46182ddbe0088
Yu YAGIHASHI <yagihashoooooo@gmail.com>
2018-10-06 17:13:11 +0900

    Removed unnecessary namespace

--- build/app/app/Http/Controllers/Api/UserController.php
+++ build/app/app/Http/Controllers/Api/UserController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Api;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Input;
