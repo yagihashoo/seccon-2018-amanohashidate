3ad2d839b9ee35faec4e05cf472da7feb7948e42
Yu YAGIHASHI <yagihashoooooo@gmail.com>
2018-11-24 23:50:07 +0900

    Updated login form

--- build/app/resources/views/login.blade.php
+++ build/app/resources/views/login.blade.php
@@ -1,11 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    <form method="post">
-        <input name="name" type="text" placeholder="username" autofocus>
-        <input name="password" type="password" placeholder="password">
-        <input type="submit" formaction="{{ route('login') }}" value="Login">
-        <input type="submit" formaction="{{ route('register') }}" value="Register">
-        @csrf
-    </form>
+    <div style="width:600px;margin:auto;margin-top:100px;font-family:monospace">
+        <h1>XSS Hell</h1>
+        <form method="post">
+            <div><label>Username:<input name="name" type="text" placeholder="username" autofocus></label></div>
+            <div><label>Password:<input name="password" type="password" placeholder="password"></label></div>
+            <div><input type="submit" formaction="{{ route('login') }}" value="Login">
+                <input type="submit" formaction="{{ route('register') }}" value="Register"></div>
+            @csrf
+        </form>
+    </div>
 @endsection
