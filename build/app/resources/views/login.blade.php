55c677964d3ce803692318370119242bad65fc53
Yu YAGIHASHI <yagihashoooooo@gmail.com>
2018-11-26 18:29:06 +0900

    Added siimple and edited login/register form

--- build/app/resources/views/login.blade.php
+++ build/app/resources/views/login.blade.php
@@ -1,14 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    <div style="width:600px;margin:auto;margin-top:100px;font-family:monospace">
-        <h1>XSS Hell</h1>
-        <form method="post">
-            <div><label>Username:<input name="name" type="text" placeholder="username" autofocus></label></div>
-            <div><label>Password:<input name="password" type="password" placeholder="password"></label></div>
-            <div><input type="submit" formaction="{{ route('login') }}" value="Login">
-                <input type="submit" formaction="{{ route('register') }}" value="Register"></div>
-            @csrf
-        </form>
-    </div>
+    <h1>XSS Hell</h1>
+    <form method="post" class="siimple-form">
+        <div class="siimple-form-field">
+            <label>Username:<input class="siimple-input siimple-input--fluid" name="name"
+                                   type="text" placeholder="username" autofocus>
+            </label>
+        </div>
+        <div class="siimple-form-field">
+            <label>Password:<input class="siimple-input siimple-input--fluid"
+                                   name="password" type="password"
+                                   placeholder="password">
+            </label>
+        </div>
+        <div class="siimple-form-field">
+            <input class="siimple-btn siimple-btn--blue" type="submit" formaction="{{ route('login') }}" value="Login">
+            <input class="siimple-btn siimple-btn--blue" type="submit" formaction="{{ route('register') }}"
+                   value="Register">
+        </div>
+        @csrf
+    </form>
 @endsection
