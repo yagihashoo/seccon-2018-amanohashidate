ec3450d1c1f11fab63a7c2ec2390cce08876e8ac
Yu YAGIHASHI <yagihashoooooo@gmail.com>
2018-11-27 01:29:59 +0900

    Added two pages

---
+++ build/app/client/pages/app/unsolved.js
@@ -0,0 +1,10 @@
+import Layout from '../../components/layout';
+import {axiosWrapper} from '../../lib/utils';
+import Router from 'next/router'
+
+const Unsolved = (props) => (
+    <Layout title={"Unsolved Challenges"}>
+    </Layout>
+)
+
+export default Unsolved;
---
+++ build/app/client/pages/app/upload.js
@@ -0,0 +1,8 @@
+import Layout from '../../components/layout';
+
+const Upload = (props) => (
+    <Layout title={"Upload Challenge"}>
+    </Layout>
+)
+
+export default Upload;
