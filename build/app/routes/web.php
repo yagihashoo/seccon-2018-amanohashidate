dffa654192d4dd8183ebe250ab56dd579800daaa
Eidwinds <eidwinds@gmail.com>
2018-12-12 20:07:57 +0900

    divided login and register.

--- build/app/routes/web.php
+++ build/app/routes/web.php
@@ -9,7 +9,7 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 use Illuminate\Support\Facades\Route;
 
@@ -17,9 +17,10 @@ Route::middleware('auth')->get('/', function () {
     return redirect('/app');
 })->name('top');
 
-Route::get('/login', function () {
-    return view('login');
-});
-$this->post('login', 'Auth\LoginController@login')->name('login');
-$this->post('register', 'Auth\RegisterController@register')->name('register');
+$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+$this->post('login', 'Auth\LoginController@login');
+
+$this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+$this->post('register', 'Auth\RegisterController@register');
+
 $this->get('logout', 'Auth\LoginController@logout')->name('logout');
