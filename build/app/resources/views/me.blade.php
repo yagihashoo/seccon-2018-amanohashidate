ca2c88087e1238ae80d4b02311e5facfe3dc8f30
Eidwinds <eidwinds@gmail.com>
2018-12-14 18:20:49 +0900

    added Uploaded Challenges.

--- build/app/resources/views/me.blade.php
+++ build/app/resources/views/me.blade.php
@@ -20,4 +20,29 @@
         <td>{{ $user->name }}</td>
     </tr>
 </table>
+<h2 class="title is-3">Uploaded Challenges</h2>
+ <table class="table is-fullwidth is-striped">
+    <thead>
+        <th>status</th>
+        <th>id</th>
+        <th>title</th>
+    </thead>
+    <tbody>
+    @foreach( $challenges as $challenge)
+        <tr>
+            <td>
+            @if( $challenge->verified === 1)
+                <span class="tag is-success">verified</span>
+            @endif
+            @if( $challenge->solved === 1)
+                <span class="tag is-warning">solved</span>
+            @endif
+            </td>
+            <td>{{ $challenge->id }}</td>
+            <td>{{ $challenge->title }}</td>
+        </tr>
+    @endforeach
+    </tbody>
+ </table>
+ {{ $challenges->links() }}
 @endsection
