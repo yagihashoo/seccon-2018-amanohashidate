41db5d5dc34cbd8f80a472ed801bea478b047267
Eidwinds <eidwinds@gmail.com>
2018-12-14 18:02:17 +0900

    fix typo.

--- build/app/app/Http/Controllers/TopController.php
+++ build/app/app/Http/Controllers/TopController.php
@@ -1,13 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Challenge;
 
 class TopController extends Controller
 {
     public function index()
     {
-        $challenges = $challenges = Challenge::where('verified', true)->orderBy('created_at', 'asc')->paginate(20);
+        $challenges = Challenge::where('verified', true)->orderBy('created_at', 'asc')->paginate(20);
 
         return view('top')->with('challenges', $challenges);
     }
