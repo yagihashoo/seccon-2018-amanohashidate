2373c55b153f7b8f41d828d19d9823ea48c827b0
Yu YAGIHASHI <yagihashoooooo@gmail.com>
2018-12-16 03:40:32 +0900

    Change title for login page

--- build/app/resources/views/layouts/auth.blade.php
+++ build/app/resources/views/layouts/auth.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Amanohashidate</title>
+    <title>XSS Hell</title>
     <link href="{{ asset('css/bulma.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
