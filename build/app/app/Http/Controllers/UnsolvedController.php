301332a92554865a209a12635b8d2938c20510c4
Yu YAGIHASHI <yagihashoooooo@gmail.com>
2018-12-21 06:21:48 +0900

    Fix unsolved not to use solved column

--- build/app/app/Http/Controllers/UnsolvedController.php
+++ build/app/app/Http/Controllers/UnsolvedController.php
@@ -8,7 +8,7 @@ class UnsolvedController extends Controller
 {
     public function index()
     {
-        $challenges = Challenge::where('status', 'VERIFIED')->where('solved', false)->orderBy('id', 'asc')->get()->toArray();
+        $challenges = Challenge::where('status', 'VERIFIED')->orderBy('id', 'asc')->get()->toArray();
 
         $res = implode("\n", array_column($challenges, 'title'));
         return response($res, 200)->header('Content-Type', 'text/plain');
