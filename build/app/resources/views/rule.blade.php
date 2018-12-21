a2075a5a4f0fbdc12a959ac9846d970341ea2e02
Yu YAGIHASHI <yagihashoooooo@gmail.com>
2018-12-21 09:20:33 +0900

    Add data: and blob: on CSP header

--- build/app/resources/views/rule.blade.php
+++ build/app/resources/views/rule.blade.php
@@ -6,7 +6,7 @@
         <li>Challenge一覧に同時に掲載される問題は1チーム1問のみです。</li>
         <li>公開中の問題がない場合のみ新しい問題を登録できます。</li>
         <li>Payloadは `location.search` 経由で渡されます。(形式: `/?${payload}` )</li>
-        <li>動作検証環境サーバではCSP( `default-src 'self' 'unsafe-inline' 'unsafe-eval'` )が有効です。</li>
+        <li>動作検証環境サーバではCSP( `default-src 'self' 'unsafe-inline' 'unsafe-eval' data: blob:` )が有効です。</li>
         <li>動作検証環境サーバのContent-Typeヘッダの設定値は `text/html; charset=utf-8` です。</li>
         <li>動作検証環境のタイムアウトは2000msです。</li>
         <li>動作検証に用いるブラウザは `Google Chrome 71.0.3578.98` です。</li>
