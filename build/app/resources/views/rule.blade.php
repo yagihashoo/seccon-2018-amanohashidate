8da44c16323115f44612afbcec1087b5f37281fb
Yu YAGIHASHI <yagihashoooooo@gmail.com>
2018-12-21 18:32:21 +0900

    Fix rule

--- build/app/resources/views/rule.blade.php
+++ build/app/resources/views/rule.blade.php
@@ -9,7 +9,7 @@
         <li>Payloadは `location.search` 経由で渡されます。(形式: `/?${payload}` )</li>
         <li>動作検証環境サーバではCSP( `default-src 'self' 'unsafe-inline' 'unsafe-eval' data: blob:` )が有効です。</li>
         <li>動作検証環境サーバのContent-Typeヘッダの設定値は `text/html; charset=utf-8` です。</li>
-        <li>動作検証環境のタイムアウトは2000msです。</li>
+        <li>動作検証環境のタイムアウトは1000msです。</li>
         <li>動作検証に用いるブラウザは `Google Chrome 71.0.3578.98` です。</li>
         <li>Defense pointの分配は<a href="{{ route('unsolved') }}">こちら</a>のページの解かれていない問題のタイトル一覧をもとにおこないます。</li>
         <li>各所にRate Limitを設定しています。ご留意ください。</li>
